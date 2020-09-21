9d661bfaa7bb26df83821434eb2029647ca8c1e1
linjianghe <linjianghe@tencent.com>
2020-09-22 07:28:26 +0800

    feat: 增加 test

--- test/scripts/index.test.ts
+++ test/scripts/index.test.ts
@@ -61,5 +61,64 @@ describe('./index.ts', () => {
       expect(expectsList.length).to.equal(tmpList.length);
       expect(tmpList).to.have.lengthOf(17);
     });
+
+    it('demo01 lcov.info 内容是一致，且不为空', () => {
+      const expectContent = fse.readFileSync(path.join(demoExpects, 'lcov.info'), { encoding: 'utf8' });
+      const tmpContent = fse.readFileSync(path.join(demoTmpOutput, 'lcov.info'), { encoding: 'utf8' });
+
+      expect(tmpContent).to.equal(expectContent);
+      expect(tmpContent).not.to.be.empty;
+    });
+  });
+
+  describe('createE2ECoverage() for demo02', () => {
+    const demoFixtures = path.join(__dirname, '../data/fixtures/demo02');
+    const demoExpects = path.join(__dirname, '../data/expects/demo02_coverage');
+    const demoTmpOutput = path.join(__dirname, '../tmp/demo02_coverage');
+
+    let result: IE2ECoverageResult;
+
+    before(async () => {
+      fse.removeSync(demoTmpOutput);
+
+      const globPattern = path.join(demoFixtures, '/**/*.json');
+      const reporterDir = path.join(demoTmpOutput);
+
+      result = await createE2ECoverage(globPattern, {
+        dir: reporterDir,
+      });
+    });
+
+    after(() => {
+      fse.removeSync(demoTmpOutput);
+    });
+
+    it('demo02 result 结果符合预期', () => {
+      expect(result).to.eql({
+        'data': {
+          'branches': 100,
+          'functions': 83.33,
+          'lines': 92.86,
+          'statements': 92.86,
+        },
+        'reporterDir': demoTmpOutput,
+      });
+    });
+
+    it('demo02 输出文件数量符合预期，为 17 个文件', () => {
+      const expectsList = glob.sync(`${demoExpects}/**`);
+      const tmpList = glob.sync(`${demoTmpOutput}/**`);
+
+      expect(expectsList.length).to.equal(tmpList.length);
+      expect(tmpList).to.have.lengthOf(13);
+    });
+
+    it('demo02 lcov.info 内容是一致，且不为空', () => {
+      const expectContent = fse.readFileSync(path.join(demoExpects, 'lcov.info'), { encoding: 'utf8' });
+      const tmpContent = fse.readFileSync(path.join(demoTmpOutput, 'lcov.info'), { encoding: 'utf8' });
+
+      expect(tmpContent).to.equal(expectContent);
+      expect(tmpContent).not.to.be.empty;
+    });
   });
 });
